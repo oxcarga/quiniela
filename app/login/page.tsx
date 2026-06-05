2658ff2632b9d30f441b5d2fcf017d391a5bb7e3
Oscar Garita <oxcargasa@gmail.com>
2026-06-05 00:11:09 -0600

    Show env vars in login screen for debuging

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,6 +12,16 @@ export default function LoginPage() {
             Ingresa tu email para recibir tu enlace de acceso
           </p>
         </div>
+        <br />
+        <p>1: {process.env.NEXT_PUBLIC_APP_URL}</p>
+        <p>2: {process.env.NEXT_PUBLIC_FIREBASE_API_KEY}</p>
+        <p>3: {process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}</p>
+        <p>4: {process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}</p>
+        <p>5: {process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}</p>
+        <p>6: {process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}</p>
+        <p>7: {process.env.NEXT_PUBLIC_FIREBASE_APP_ID}</p>
+        <p>8: {process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}</p>
+        <br />
         <LoginForm />
       </div>
     </main>
