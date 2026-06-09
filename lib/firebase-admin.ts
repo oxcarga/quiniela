bfbca9dee734f715a54e1de9c3e345bfbe7e7da8
Oscar Garita <oxcargasa@gmail.com>
2026-06-09 10:35:56 -0600

    Init firebase for local setup properly

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,9 +2,13 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 if (!getApps().length) {
-  initializeApp({
-    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)),
-  });
+  if (process.env.FIREBASE_AUTH_EMULATOR_HOST) {
+    initializeApp({ projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID });
+  } else {
+    initializeApp({
+      credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)),
+    });
+  }
 }
 
 export const adminAuth = getAuth();
